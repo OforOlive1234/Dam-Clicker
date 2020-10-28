72a9022b49f530bb38b13dad9429f7b0900507a8
OforOlive1234 <69664883+OforOlive1234@users.noreply.github.com>
2020-10-28 11:39:47 +0800

    Basic Levelling System Added

--- App/Code/JS/game.js
+++ App/Code/JS/game.js
@@ -5,12 +5,17 @@ var auto_clicks = 0;
 var click_rate = 1000;
 var increment = 1;
 var lvl = 1;
+var lvl_upgrade_value = lvl*50
 
 
 //Functions
 
 function oncl() {
     clicks += increment;
+    if (clicks == lvl_upgrade_value) {
+        lvl++
+    }
+
     textRefresh();
 }
 

edbeb6d37f530a8bf4c98b619b74361aedd1e443
OforOlive1234 <69664883+OforOlive1234@users.noreply.github.com>
2020-10-29 12:00:21 +0800

    POggY

--- App/Code/JS/game.js
+++ App/Code/JS/game.js
@@ -5,14 +5,14 @@ var auto_clicks = 0;
 var click_rate = 1000;
 var increment = 1;
 var lvl = 1;
-var lvl_upgrade_value = lvl*50
+var lvl_upgrade_value = lvl*50;
 
 
 //Functions
 
 function oncl() {
     clicks += increment;
-    if (clicks == lvl_upgrade_value) {
+    if (clicks >= lvl_upgrade_value) {
         lvl += 1;
     }
 

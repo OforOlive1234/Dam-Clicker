b0a29cb7dc8e78edf4b2e93853b8eb08ed76988e
OforOlive1234 <69664883+OforOlive1234@users.noreply.github.com>
2020-10-30 19:57:27 +0800

    Adding AutoClicker

--- App/Code/JS/game.js
+++ App/Code/JS/game.js
@@ -2,6 +2,8 @@
 
 var clicks = 0;
 var auto_clicks = 0;
+var total_clicks = clicks + auto_clicks;
+var auto_clicks_increment = 1000/auto_clicks;
 var click_rate = 1000;
 var increment = 1;
 var lvl = 1;
@@ -10,18 +12,25 @@ var lvl_upgrade_value = lvl*50;
 
 //Functions
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 function oncl() {
     clicks += increment;
-    if (clicks >= lvl_upgrade_value)
-        lvl += 1;
+    if (clicks >= lvl_upgrade_value) {
+        lvl = lvl + 1;
     }
     textRefresh();
 }
 
 function textRefresh() {
-    document.getElementById("scoreDisplay").innerHTML=clicks+" Points";
+    document.getElementById("scoreDisplay").innerHTML=total_clicks+" Points";
 }
 
-function lvlRefresh() {
-        document.getElementById("levelDisplay").innerHTML=lvl;
+function autoClicker() {
+    while (auto_clicks != 0) {
+        await sleep(click_rate);
+        auto_clicks += auto_clicks_increment;
+    }
 }

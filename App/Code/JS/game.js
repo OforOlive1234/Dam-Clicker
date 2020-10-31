5233a9f16f6ab1699e8fd8c56ffe181ae44f3b6c
OforOlive1234 <69664883+OforOlive1234@users.noreply.github.com>
2020-10-31 10:26:42 +0800

    Auto clicker didn't work :c

--- App/Code/JS/game.js
+++ App/Code/JS/game.js
@@ -2,7 +2,6 @@
 
 var clicks = 0;
 var auto_clicks = 0;
-var total_clicks = clicks + auto_clicks;
 var auto_clicks_increment = 1000/auto_clicks;
 var click_rate = 1000;
 var increment = 1;
@@ -11,10 +10,6 @@ var lvl_upgrade_value = lvl*50;
 
 
 //Functions
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
 
 function oncl() {
     clicks += increment;
@@ -25,12 +20,5 @@ function oncl() {
 }
 
 function textRefresh() {
-    document.getElementById("scoreDisplay").innerHTML=total_clicks+" Points";
-}
-
-function autoClicker() {
-    while (auto_clicks != 0) {
-        await sleep(click_rate);
-        auto_clicks += auto_clicks_increment;
-    }
+    document.getElementById("scoreDisplay").innerHTML=clicks+" Points";
 }
